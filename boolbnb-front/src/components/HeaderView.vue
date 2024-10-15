347fbe7ee51983dd9aa9bb5897b4f292448dc20e
LorisGalleana <loris.galleana@gmail.com>
2024-10-15 14:10:58 +0200

    first page

---
+++ boolbnb-front/src/components/HeaderView.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: "HeaderView",
+}
+</script>
+
+<template>
+    <nav class="d-flex">
+        <RouterLink to="/ricerca-avanzata">Cerca</RouterLink>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Accedi
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Login</a></li>
+                <li><a class="dropdown-item" href="#">Registrati</a></li>
+            </ul>
+        </div>
+      </nav>
+</template>
+
+
+<style lang="scss" scoped>
+@use "../styles/general.scss" as *;
+</style>
+
---
+++ boolbnb-front/src/components/pages/HomeView.vue
@@ -0,0 +1,20 @@
+<script >
+
+</script>
+
+<template>
+  <main>
+    <div class="d-flex justify-content-center">
+      <div>
+        <input type="search" name="" id="" >
+        <h1>SEI NELLA HOME</h1>
+      </div>
+    </div>
+  </main>
+</template>
+
+
+<style lang="scss" scoped>
+@use "../../styles/general.scss" as *;
+
+</style>
