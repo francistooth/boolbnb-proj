5a6efe5cac94c76a11c71e1d8efc4ae32e8847ed
battistaennio <ennio.battista99@gmail.com>
2024-10-15 15:10:07 +0200

    style: add title BoolBnB in head->title

--- boolbnb-back/resources/views/auth/register.blade.php
+++ boolbnb-back/resources/views/auth/register.blade.php
@@ -109,7 +109,7 @@
                             <div class="mb-4 row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Register') }}
+                                        {{ __('Registrati') }}
                                     </button>
                                 </div>
                             </div>
