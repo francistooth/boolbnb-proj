20952a2e55a78338fcabb039535a56216598bb6b
battistaennio <ennio.battista99@gmail.com>
2024-10-15 12:57:24 +0200

    feat: update user parameters for login

--- boolbnb-back/app/Http/Controllers/Auth/RegisteredUserController.php
+++ boolbnb-back/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,15 +31,19 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'name' => ['string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'surname' => ['string', 'max:255'],
+            'date_birth' => ['date', 'date_format:Y-m-d']
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'surname' => $request->surname,
+            'date_birth' => $request->date_birth,
         ]);
 
         event(new Registered($user));
