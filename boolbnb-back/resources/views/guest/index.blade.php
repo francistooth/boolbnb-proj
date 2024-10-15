8db004caaba584271acad9038b6c0e20b5ffc14e
battistaennio <ennio.battista99@gmail.com>
2024-10-15 15:54:52 +0200

    style: remove bg-color

--- boolbnb-back/resources/views/guest/index.blade.php
+++ boolbnb-back/resources/views/guest/index.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.guest')
 @section('content')
-<body class="font-sans text-gray-900 antialiased">
-    <div class="jumbotron p-5 mb-4 bg-light rounded-3">
-        <div class="container py-5">
-            <h1 class="display-5 fw-bold">
-                Welcome to BoolBnB
-            </h1>
 
-            <p class="col-md-8 fs-4">
-                content
-            </p>
-            <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg" type="button">Filter</a>
+    <body class="font-sans text-gray-900 antialiased">
+        <div class="jumbotron p-5 mb-4 rounded-3">
+            <div class="container py-5">
+                <h1 class="display-5 fw-bold">
+                    Welcome to BoolBnB
+                </h1>
+
+                <p class="col-md-8 fs-4">
+                    content
+                </p>
+                <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg"
+                    type="button">Filter</a>
+            </div>
         </div>
-    </div>
 
-    <div class="content">
-        <div class="container">
+        <div class="content">
+            <div class="container">
 
+            </div>
         </div>
-    </div>
-</body>
+    </body>
 @endsection
