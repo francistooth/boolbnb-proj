9ad79e53ce510db2653f6b61024d48936601d591
battistaennio <ennio.battista99@gmail.com>
2024-10-15 12:22:22 +0200

    feat: add configs

---
+++ boolbnb-back/config/apartments.php
@@ -0,0 +1,163 @@
+<?php
+
+return [
+    [
+        'title' => 'Appartamento a Pigneto',
+        'description' => 'Un bellissimo appartamento situato nel cuore di Pigneto.',
+        'address' => 'Via del Corso, 73, Roma',
+    ],
+    [
+
+        'title' => 'Attico vicino al Colosseo',
+        'description' => 'Splendido attico con vista sul Colosseo.',
+        'address' => 'Piazza Navona, 2, Roma',
+    ],
+    [
+
+        'title' => 'Bilocale a San Giovanni',
+        'description' => 'Accogliente bilocale vicino a Piazza San Giovanni.',
+        'address' => 'Via della Scrofa, 117, Roma',
+    ],
+    [
+
+        'title' => 'Monolocale a Tiburtina',
+        'description' => 'Moderno monolocale vicino alla stazione Tiburtina.',
+        'address' => 'Via dei Giardinetti, 46, Roma',
+    ],
+    [
+        'title' => 'Trilocale a Prati',
+        'description' => 'Elegante trilocale nel quartiere Prati, a due passi da San Pietro.',
+        'address' => 'Via di Porta Lavernale, 7, Roma',
+    ],
+    [
+        'title' => 'Quadrilocale a Testaccio',
+        'description' => 'Spazioso appartamento situato a Testaccio, ideale per famiglie.',
+        'address' => 'Piazza di Spagna, 88, Roma',
+    ],
+    [
+        'title' => 'Appartamento nel quartiere Monti',
+        'description' => 'Appartamento accogliente nel famoso quartiere Monti.',
+        'address' => 'Via del Boccaccio, 25, Roma',
+
+    ],
+    [
+        'title' => 'Loft moderno a Termini',
+        'description' => 'Un loft moderno a pochi passi dalla stazione Termini.',
+        'address' => 'Via del Castro Pretorio, 30, Roma',
+    ],
+    [
+        'title' => 'Bilocale a Ostiense',
+        'description' => 'Accogliente bilocale nella vivace zona Ostiense.',
+        'address' => 'Via Margutta, 54, Roma',
+    ],
+    [
+        'title' => 'Appartamento a Flaminio',
+        'description' => 'Luminoso appartamento vicino al MAXXI, nel quartiere Flaminio.',
+        'address' => 'Via dei Fienaroli, 30, Roma',
+    ],
+    [
+
+        'title' => 'Attico di lusso',
+        'description' => 'Attico moderno con vista panoramica nel quartiere EUR.',
+        'address' => 'Via di Ripetta, 36, Roma',
+    ],
+    [
+
+        'title' => 'Appartamento a Salario',
+        'description' => 'Comodo appartamento in una zona tranquilla vicino a Villa Ada.',
+        'address' => 'Lungotevere Sanzio Raffaello,9, Roma',
+    ],
+    [
+        'title' => 'Bilocale a Casal Bruciato',
+        'description' => 'Bilocale pratico e ben collegato ai mezzi pubblici.',
+        'address' => 'Via dei Serpenti, 6, Roma',
+    ],
+    [
+
+        'title' => 'Trilocale a San Lorenzo',
+        'description' => 'Trilocale luminoso nel quartiere di San Lorenzo, vicino all’università.',
+        'address' => 'Via dei Giubbonari, 23, Roma',
+
+    ],
+    [
+        'title' => 'Appartamento a Cinecittà',
+        'description' => 'Spazioso appartamento a Cinecittà, vicino alla metro.',
+        'address' => 'Piazza di San Marco, 49, Roma',
+    ],
+    [
+        'title' => 'Appartamento al Gianicolo',
+        'description' => 'Appartamento panoramico al Gianicolo, perfetto per le coppie.',
+        'address' => 'Via di San Giovanni in Laterano, 4, Roma',
+    ],
+    [
+        'title' => 'Monolocale a Campo Marzio',
+        'description' => 'Accogliente monolocale nel centro di Roma, a Campo Marzio.',
+        'address' => 'Via di Campo Marzio, 70, Roma',
+    ],
+    [
+        'title' => 'Attico a Garbatella',
+        'description' => 'Attico panoramico in uno dei quartieri più caratteristici di Roma.',
+        'address' => 'Via delle Fornaci, 203, Roma',
+    ],
+    [
+        'title' => 'Appartamento a Trastevere ',
+        'description' => 'Secondo appartamento a Trastevere, con vista sul Tevere.',
+        'address' => 'Via della Vite, 39, Roma',
+    ],
+    [
+        'title' => 'Monolocale vicino al Colosseo ',
+        'description' => 'Secondo monolocale vicino al Colosseo, con ottimi collegamenti.',
+        'address' => 'Via di Torre Argentina, 42, Roma',
+
+    ],
+    [
+        'title' => 'Appartamento nel Quartiere Coppedè',
+        'description' => 'Affascinante appartamento nel suggestivo Quartiere Coppedè.',
+        'address' => 'Via Tagliamento, 76, Roma',
+    ],
+    [
+        'title' => 'Monolocale a San Giovanni',
+        'description' => 'Monolocale moderno a pochi passi dalla Basilica di San Giovanni.',
+        'address' => 'Via Amba Aradam, 23, Roma',
+    ],
+    [
+        'title' => 'Bilocale a Porta Maggiore',
+        'description' => 'Bilocale comodo con ottimi collegamenti al centro città.',
+        'address' => 'Piazza Porta Maggiore, 14, Roma',
+    ],
+    [
+        'title' => 'Attico a Monteverde',
+        'description' => 'Elegante attico con terrazza panoramica in Monteverde.',
+        'address' => 'Viale di Villa Pamphili, 139, Roma',
+    ],
+    [
+        'title' => 'Appartamento a Ostiense',
+        'description' => 'Appartamento moderno situato nel vivace quartiere Ostiense.',
+        'address' => 'Via del Gazometro, 38, Roma',
+    ],
+    [
+        'title' => 'Trilocale a Tiburtina',
+        'description' => 'Spazioso trilocale vicino alla stazione Tiburtina.',
+        'address' => 'Via Tiburtina, 755, Roma',
+    ],
+    [
+        'title' => 'Monolocale a Quarticciolo',
+        'description' => 'Accogliente monolocale in un quartiere tranquillo.',
+        'address' => 'Via dei Castani, 94, Roma',
+    ],
+    [
+        'title' => 'Appartamento a Casalotti',
+        'description' => 'Appartamento luminoso con giardino privato a Casalotti.',
+        'address' => 'Via di Casalotti, 229, Roma',
+    ],
+    [
+        'title' => 'Bilocale a Centocelle',
+        'description' => 'Bilocale recentemente ristrutturato in una zona vivace.',
+        'address' => 'Via Carlo Bartolomeo Piazza, 24, Roma',
+    ],
+    [
+        'title' => 'Attico a Prati Fiscali',
+        'description' => 'Attico con vista spettacolare, perfetto per famiglie.',
+        'address' => 'Via Val Maggia, 135, Roma',
+    ],
+];
---
+++ boolbnb-back/config/sponsors.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    [
+        'title' => 'Bronze',
+        'price' => 2.99,
+        'duration' => '24 ore',
+    ],
+    [
+        'title' => 'Silver',
+        'price' => 5.99,
+        'duration' => '72 ore',
+    ],
+    [
+        'title' => 'Gold',
+        'price' => 9.99,
+        'duration' => '144 ore',
+    ],
+];
