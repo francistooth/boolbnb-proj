2bb0b35567f05b019205c47f3395d4299c0e56f7
battistaennio <ennio.battista99@gmail.com>
2024-10-15 14:24:09 +0200

    feat: add italian errors message in RegisteredUserController

--- boolbnb-back/app/Http/Controllers/Auth/RegisteredUserController.php
+++ boolbnb-back/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -30,20 +31,45 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
-            'name' => ['string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'surname' => ['string', 'max:255'],
-            'date_birth' => ['date', 'date_format:Y-m-d']
-        ]);
+        $request->validate(
+            [
+                'name' => ['string', 'max:50'],
+                'email' => ['required', 'string', 'lowercase', 'email', 'max:150', 'unique:' . User::class],
+                'password' => ['required', 'confirmed', 'min:8', Rules\Password::defaults()],
+                'surname' => ['string', 'max:50'],
+                'date_birth' => ['date', 'date_format:d/m/Y']
+            ],
+
+            [
+                'name.string' => 'Il campo nome deve essere una stringa',
+                'name.max' => 'Il campo nome può contenere massimo :max caratteri',
+
+                'email.required' => 'Il campo email è obbligatorio',
+                'email.string' => 'Il campo email deve essere una stringa',
+                'email.lowercase' => 'Il campo email deve contenere caratteri minuscoli',
+                'email.email' => 'Il campo email deve essere una email valida',
+                'email.max' => 'Il campo email può contenere massimo :max caratteri',
+                'email.unique' => 'La email inserita è già stata utilizzata',
+
+                'password.required' => 'Il campo password è obbligatorio',
+                'password.confirmed' => 'Le password devono combaciare',
+                'password.min' => 'Il campo password deve contenere almeno :min caratteri',
+
+                'surname.string' => 'Il campo cognome deve essere una stringa',
+                'surname.max' => 'Il campo cognome può contenere massimo :max caratteri',
+
+                'date_birth.date' => 'Il campo Data di Nascita deve essere una data',
+                'date_birth.date_format' => 'Il campo Data di Nascita deve essere del seguente formato GG/MM/AAAA',
+            ]
+
+        );
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'surname' => $request->surname,
-            'date_birth' => $request->date_birth,
+            'date_birth' => date("Y-m-d", strtotime($request->date_birth)),
         ]);
 
         event(new Registered($user));
