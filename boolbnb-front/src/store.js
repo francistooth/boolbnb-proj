b2cbc7dab1a195f70950c4f788c2f0da38708d04
LorisGalleana <loris.galleana@gmail.com>
2024-10-15 17:07:17 +0200

    un bel commentino per franceschino

---
+++ boolbnb-front/src/store.js
@@ -0,0 +1,13 @@
+import {reactive} from 'vue';
+
+export const store = reactive({
+    apartments:
+        {
+            title: 'Appartamento a Pigneto',
+            description: 'Un bellissimo appartamento situato nel cuore di Pigneto.',
+            address: 'Via del Corso, 73, Roma',
+        }
+    
+
+
+})
