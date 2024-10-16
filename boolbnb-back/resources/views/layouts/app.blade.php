77dc16bdaa71b5d9d12e739421d624a3ca599bbb
alessandrofogliada <fogliada.a@gmail.com>
2024-10-16 17:02:58 +0200

    fix: card in ordine

--- boolbnb-back/resources/views/layouts/app.blade.php
+++ boolbnb-back/resources/views/layouts/app.blade.php
@@ -74,7 +74,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     {{-- <a class="dropdown-item" href="{{ route('admin.home') }}">{{ __('Dashboard') }}</a> --}}
-                                    <a class="dropdown-item" href="{{ route('admin.apartments.index') }}">{{ __('Dashboard') }}</a>
+                                    <a class="dropdown-item" href="{{ route('admin.apartments.index') }}">I miei appartamenti</a>
                                     <a class="dropdown-item" href="{{ route('admin.user.show' , Auth::id()) }}">Il mio account</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
