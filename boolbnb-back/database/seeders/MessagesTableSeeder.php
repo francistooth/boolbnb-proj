e257b1f6c135d801486f2190730df7b2b8a3e937
Nicolò Pincelli <pincelli.n@gmail.com>
2024-10-16 15:49:45 +0200

    Fix apartment_id seeder massage

--- boolbnb-back/database/seeders/MessagesTableSeeder.php
+++ boolbnb-back/database/seeders/MessagesTableSeeder.php
@@ -12,18 +12,17 @@ class MessagesTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-        public function run(Faker $faker): void
-        {
-            $numberOfMessages = 30;
-    
-            for ($i = 0; $i < $numberOfMessages; $i++) {
-                $new_message = new Message();
-                $new_message->apartment_id = $faker->numberBetween(0,30);
-                $new_message->name = $faker->name();
-                $new_message->email = $faker->safeEmail();
-                $new_message->message = $faker->sentence();
-                $new_message->save();
-            }
-        }
-}
+    public function run(Faker $faker): void
+    {
+        $numberOfMessages = 30;
 
+        for ($i = 0; $i < $numberOfMessages; $i++) {
+            $new_message = new Message();
+            $new_message->apartment_id = $faker->numberBetween(1, 30);
+            $new_message->name = $faker->name();
+            $new_message->email = $faker->safeEmail();
+            $new_message->message = $faker->sentence();
+            $new_message->save();
+        }
+    }
+}
