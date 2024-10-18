6c5bcb4bc2fbf3f57f4950ff5366966776533449
battistaennio <ennio.battista99@gmail.com>
2024-10-18 14:56:28 +0200

    feat: add base form structure

--- boolbnb-front/src/components/partials/Mail.vue
+++ boolbnb-front/src/components/partials/Mail.vue
@@ -1,12 +1,61 @@
 <script>
 
 export default {
-    name: 'mail'
+    name: 'mail',
+
+    data(){
+        return{
+            email: 'pino@gmail.it',
+            message: 'Ciao sono Pino',
+        }
+    },
+
+    methods: {
+
+    }
+
 }
 </script>
 
 <template>
-    <h1>PROVA</h1>
+
+    <form action="#">
+
+        <div>
+            <label for="email" class="form-label">Email:</label>
+            <input v-model="email" type="email" id="email" class="form-control">
+        </div>
+
+        <div>
+            <label for="message" class="form-label">Messaggio:</label>
+            <textarea v-model="message" rows="8" id="message" class="form-control"></textarea>
+        </div>
+
+        <div>
+            <button type="submit" class="btn btn-success">Invia</button>
+        </div>
+
+    </form>
+
 </template>
 
-<script lang="scss" scoped></script>
+<style lang="scss" scoped>
+
+form{
+    margin: 50px auto;
+    width: 50%;
+
+    div{
+        margin-bottom: 20px;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+
+        label{
+            width: 100%;
+            text-align: center;
+        }
+    }
+}
+
+</style>
