a5aed2d2355d1f5da4e255970f8b9fa01539c2f1
battistaennio <ennio.battista99@gmail.com>
2024-10-18 11:13:43 +0200

    style: add default img if apartment hasn't img

--- boolbnb-back/resources/views/admin/apartments/index.blade.php
+++ boolbnb-back/resources/views/admin/apartments/index.blade.php
@@ -4,7 +4,8 @@
     <div class="container d-flex justify-content-between align-items-center mt-4">
         <h2> Dashboard di {{ Auth::user()->name }} {{ Auth::user()->surname }} </h2>
         <div>
-            <a class="btn btn-success" href="{{ route('admin.apartments.create') }}"> Aggiungi un appartamento <i class="fa-solid fa-plus"> </i></a>
+            <a class="btn btn-success" href="{{ route('admin.apartments.create') }}"> Aggiungi un appartamento <i
+                    class="fa-solid fa-plus"> </i></a>
         </div>
     </div>
     <div class="container">
@@ -13,13 +14,9 @@
                 {{ session('delete') }}
             </div>
         @endif
-        
-        <div
-            class="table-responsive mt-5"
-        >
-            <table
-                class="table rounded"
-            >
+
+        <div class="table-responsive mt-5">
+            <table class="table rounded">
                 <thead>
                     <tr>
                         <th scope="col"> Immagine </th>
@@ -32,38 +29,33 @@
                 <tbody>
                     @foreach ($apartments as $apartment)
                         <tr class="">
-                                <td class="w-25"> 
-                                  <a href="{{route('admin.apartments.show', $apartment)}}">
-                                      <img
-                                      class="w-50 rounded"
-                                      src="{{ asset('storage/' . $apartment->img_path)  }}"
-                                      alt="{{ $apartment->img_name }}">
-                                  </a>
-                                </td> 
-                                <td class=""> {{ $apartment->title }} </td>
-                                <td> {{ $apartment->sponsors->count() }} </td>
-                                <td> {{ $apartment->is_visible ? 'Si' : 'No' }} </td>
-                                <td class="w-10">
-                                    <div class="d-flex align-items-center gap-3 flex-wrap">
-                                        <a
-                                          class="btn btn-primary"
-                                          href="{{ route('admin.apartments.edit', $apartment) }}">
-                                          <i
-                                          class="fa-solid fa-pen">
-                                          </i>
-                                        </a>
-                                        <button class="btn btn-primary"><a href="{{ route('admin.sponsor.index') }}"><i
-                                            class="fa-solid fa-sack-dollar"></i></a></button>
-                                        <form
-                                            class="d-flex align-items-center border-0 m-2 w-25"
-                                            action="{{ route('admin.apartments.destroy', $apartment) }}" method="POST"
-                                            onsubmit="return confirm('sei sicuro di voler cancellare {{ $apartment['title'] }}')">
-                                            @csrf
-                                            @method('DELETE')
-                                                <button type="submit" class="btn btn-danger rounded z-3"> <i class="text-light fa-solid fa-trash"> </i></button>
-                                        </form>
-                                    </div> 
-                                </td>
+                            <td class="w-25">
+                                <a href="{{ route('admin.apartments.show', $apartment) }}">
+                                    <img class="w-50 rounded" src="{{ asset('storage/' . $apartment->img_path) }}"
+                                        alt="{{ $apartment->img_name }}" onerror="this.src='/img/default-image.jpg'">
+                                </a>
+                            </td>
+                            <td class=""> {{ $apartment->title }} </td>
+                            <td> {{ $apartment->sponsors->count() }} </td>
+                            <td> {{ $apartment->is_visible ? 'Si' : 'No' }} </td>
+                            <td class="w-10">
+                                <div class="d-flex align-items-center gap-3 flex-wrap">
+                                    <a class="btn btn-primary" href="{{ route('admin.apartments.edit', $apartment) }}">
+                                        <i class="fa-solid fa-pen">
+                                        </i>
+                                    </a>
+                                    <button class="btn btn-primary"><a href="{{ route('admin.sponsor.index') }}"><i
+                                                class="fa-solid fa-sack-dollar"></i></a></button>
+                                    <form class="d-flex align-items-center border-0 m-2 w-25"
+                                        action="{{ route('admin.apartments.destroy', $apartment) }}" method="POST"
+                                        onsubmit="return confirm('sei sicuro di voler cancellare {{ $apartment['title'] }}')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger rounded z-3"> <i
+                                                class="text-light fa-solid fa-trash"> </i></button>
+                                    </form>
+                                </div>
+                            </td>
                         </tr>
                     @endforeach
 
@@ -73,6 +65,6 @@
                 </tbody>
             </table>
         </div>
-        
+
     </div>
 @endsection
