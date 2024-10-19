03a72ee073315915d06a954cbcf73de97419e612
Nicolò Pincelli <pincelli.n@gmail.com>
2024-10-19 15:09:50 +0200

    pre-styling

--- boolbnb-front/src/components/HeaderView.vue
+++ boolbnb-front/src/components/HeaderView.vue
@@ -18,6 +18,7 @@ export default {
             </ul>
         </div>
     </nav>
+
 </template>
 
 
--- boolbnb-front/src/components/general/ApartmentCard.vue
+++ boolbnb-front/src/components/general/ApartmentCard.vue
@@ -25,12 +25,30 @@ export default {
 
 <template>
     <!-- <RouterLink to="/dettagli-appartamento"><img src="/img/glamp1.jpeg" class="card-img-top" alt="..."></RouterLink> -->
-    <div class="card" style="width: 18rem;">
+    <!--  <div class="card" style="width: 18rem;">
         <img :src="data.img_path" class="card-img-top box-img" :alt="data.img_name">
         <div class="card-body">
             <h5 class="card-title">{{ data.title }}</h5>
             <p class="card-text">{{ data.address }}</p>
         </div>
+    </div> -->
+    <div class="container">
+        <!-- <section class="mx-auto my-5" style="max-width: 23rem;"> -->
+
+        <div class="card booking-card v-2 mt-2 mb-4 rounded-bottom">
+            <div class="bg-image hover-overlay ripple ripple-surface ripple-surface-light"
+                data-mdb-ripple-color="light">
+                <img :src="data.img_path" :alt="data.img_name" class="img-fluid">
+            </div>
+            <div class="card-body">
+                <h4 class="card-title font-weight-bold">{{ data.title }}</h4>
+                <p class="card-text">{{ data.address }}</p>
+                <hr class="my-4">
+
+            </div>
+        </div>
+
+        <!-- </section> -->
     </div>
 </template>
 
@@ -46,5 +64,29 @@ ul {
     width: 200px;
     height: 200px;
 
+}
+
+body {
+    background-color: #f5f7fa;
+}
+
+.card.booking-card.v-2 {
+    background-color: #c7f2e3;
+}
+
+.card.booking-card.v-2 .fa {
+    color: #f7aa00;
+}
+
+.card.booking-card.v-2 .card-body .card-text {
+    color: #db2d43;
+}
+
+.card.card.booking-card.v-2 .chip {
+    background-color: #87e5da;
+}
+
+.card.booking-card.v-2 .card-body hr {
+    border-top: 1px solid #f7aa00;
 }
 </style>
