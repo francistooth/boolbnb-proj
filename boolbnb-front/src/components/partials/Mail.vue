07db5d8d95ec3cf22a1340125d76166d80f84c05
battistaennio <ennio.battista99@gmail.com>
2024-10-21 11:20:46 +0200

    feat: integrate loader

--- boolbnb-front/src/components/partials/Mail.vue
+++ boolbnb-front/src/components/partials/Mail.vue
@@ -1,9 +1,13 @@
 <script>
 import axios from 'axios';
 import { store } from '../../store';
+import Loader from './Loader.vue';
 
 export default {
     name: 'mail',
+    components: {
+        Loader
+    },
     props: {
         slug: String,
     },
@@ -13,6 +17,13 @@ export default {
             name: 'Pino',
             email: 'pino@gmail.it',
             message: 'Ciao sono Pino',
+            sending: false,
+
+            errors:{
+                email: [],
+                message: [],
+                name: [],
+            },
         }
     },
 
@@ -20,6 +31,7 @@ export default {
 
         sendForm() {
 
+            this.sending = true;
             const data = {
                 name: this.name,
                 email: this.email,
@@ -29,7 +41,20 @@ export default {
 
             axios.post(store.apiUrl + 'messaggi', data)
                 .then(response => {
+
                     console.log(response.data);
+
+                    if(!response.data.success){
+                        this.errors = response.data.errors;
+                    } else {
+                        this.errors = {
+                            name: [],
+                            email: [],
+                            message: [],
+                        }
+                    }
+
+                    this.sending = false;
                 })
                 .catch(error => {
                     console.log(error.message);
@@ -41,34 +66,46 @@ export default {
 
 <template>
 
-    <form action="#" @submit.prevent="sendForm">
+    <section>
+        <form v-if="!sending" action="#" @submit.prevent="sendForm">
 
-        <div class="row">
-            <div class="col">
+            <div>
                 <label for="name" class="form-label">Nome:</label>
+                <p class="text-danger">{{ errors.name?.toString() }}</p>
                 <input v-model="name" type="text" id="name" class="form-control">
             </div>
 
-            <div class="col">
+            <div>
                 <label for="email" class="form-label">Email:</label>
+                <p class="text-danger">{{ errors.email?.toString() }}</p>
                 <input v-model="email" type="email" id="email" class="form-control">
             </div>
-        </div>
 
-        <div>
-            <label for="message" class="form-label">Messaggio:</label>
-            <textarea v-model="message" rows="8" id="message" class="form-control"></textarea>
-        </div>
+            <div>
+                <label for="message" class="form-label">Messaggio:</label>
+                <p class="text-danger">{{ errors.message?.toString() }}</p>
+                <textarea v-model="message" rows="8" id="message" class="form-control"></textarea>
+            </div>
 
-        <div>
-            <button type="submit" class="btn btn-success">Invia</button>
-        </div>
+            <div>
+                <button type="submit" class="btn btn-success">Invia</button>
+            </div>
 
-    </form>
+        </form>
+
+        <Loader v-else/>
+
+    </section>
+
 
 </template>
 
 <style lang="scss" scoped>
+
+section{
+    height: 450px;
+}
+
 form {
     margin: 50px auto;
     width: 50%;
