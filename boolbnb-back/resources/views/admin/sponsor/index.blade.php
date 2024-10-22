9210a5c34759c2e0c50db19e803e7c54faf93cae
battistaennio <ennio.battista99@gmail.com>
2024-10-22 12:52:00 +0200

    style: fix card position

--- boolbnb-back/resources/views/admin/sponsor/index.blade.php
+++ boolbnb-back/resources/views/admin/sponsor/index.blade.php
@@ -1,14 +1,17 @@
 @extends('admin.index')
 
 @section('user')
-    @foreach ($sponsors as $sponsor)
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">{{ $sponsor->title }}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">{{ $sponsor->price }}</h6>
-                <p class="card-text">{{ $sponsor->duration }}</p>
-                <a class="btn btn-secondary text-white" href="#" class="card-link">Acquista</a>
+    <div class="row justify-content-center">
+        @foreach ($sponsors as $sponsor)
+            <div class="card m-5 text-center" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $sponsor->title }}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{{ $sponsor->price }}</h6>
+                    <p class="card-text">{{ $sponsor->duration }}</p>
+                    <a class="btn btn-secondary text-white" href="#" class="card-link">Acquista</a>
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+
+    </div>
 @endsection
