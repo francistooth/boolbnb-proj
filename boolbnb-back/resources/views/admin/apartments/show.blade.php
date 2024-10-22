efe8ea8547759076eb9f1f6d78dd121e48be5e00
battistaennio <ennio.battista99@gmail.com>
2024-10-23 00:26:44 +0200

    fix: sponsor duration, sponsor price

--- boolbnb-back/resources/views/admin/apartments/show.blade.php
+++ boolbnb-back/resources/views/admin/apartments/show.blade.php
@@ -96,8 +96,8 @@
                             <div class="card m-5 text-center" style="width: 18rem;">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $sponsor->title }}</h5>
-                                    <h6 class="card-subtitle mb-2 text-muted">{{ $sponsor->price }}</h6>
-                                    <p class="card-text">{{ $sponsor->duration }}</p>
+                                    <h6 class="card-subtitle mb-2 text-muted">{{ $sponsor->price }} €</h6>
+                                    <p class="card-text">{{ $sponsor->duration }} ore</p>
 
                                     <form action="{{ route('admin.payment.store', $apartment->id) }}" method="POST">
                                         @csrf
