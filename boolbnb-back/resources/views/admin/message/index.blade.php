a2304dc0dc2107a6d8ca09f3b98b0584335ddcf9
battistaennio <ennio.battista99@gmail.com>
2024-10-24 15:46:35 +0200

    feat: add messages accordions

--- boolbnb-back/resources/views/admin/message/index.blade.php
+++ boolbnb-back/resources/views/admin/message/index.blade.php
@@ -7,53 +7,81 @@
         </div>
     @endif
 
-    <div class="container d-flex justify-content-between align-items-center mt-4">
+    <div class="container d-flex justify-content-between align-items-center mt-4 mb-4">
         <h2 class="text-secondary"> Messaggi: </h2>
     </div>
 
-    @if (!$messages)
+    @if (!$messages || count($messages) === 0)
         <div class="alert alert-dark text-center mt-5">
-            I tuoi appartamenti non hanno ricevuto messaggi.
+            Non ci sono messaggi per i tuoi appartamenti.
         </div>
     @else
         <div class="container-fluid overflow-auto">
-            <h2 class="mt-3 text-secondary"> Tutti i messaggi di {{ Auth::user()->name }} {{ Auth::user()->surname }} </h2>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Indirizzo Mittente</th>
-                        <th scope="col">Nome Mittente</th>
-                        <th scope="col">Appartamento id</th>
-                        <th scope="col">Testo</th>
-                        <th scope="col">Data</th>
-                        <th scope="col">Azione</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($messages as $message)
-                        <tr>
+            @foreach ($messages as $message)
+                <!-- modale -->
+                <div class="modal fade" id="deleteModal-{{ $message['received']->id }}" tabindex="-1"
+                    aria-labelledby="exampleModalLabel" aria-hidden="true" style="z-index: 1000">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Conferma
+                                    Eliminazione</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                Sei sicuro di voler eliminare il messaggio di "{{ $message['received']->email }}"?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
 
-                            <td>{{ $message['email'] }}</td>
-                            <td>{{ $message['name'] }}</td>
-                            <td>{{ $message['apartment_id'] }}</td>
-                            <td>{{ $message['message'] }}</td>
-                            <td>{{ date('d-m-Y', strtotime($message['created_at'])) }}</td>
-
-
-                            <td>
-                                <form action="{{ route('admin.message.destroy', $message['id']) }}" method="POST"
-                                    onsubmit="return confirm('Sei sicuro di voler cancellare il messaggio di {{ $message['name'] }} ?')">
+                                <!-- form eliminazione -->
+                                <form action="{{ route('admin.message.destroy', $message['received']) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">
-                                        <i class="text-light fa-solid fa-trash"></i>
-                                    </button>
+                                    <button type="submit" class="btn btn-danger">Conferma</button>
                                 </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapse-{{ $message['received']->id }}" aria-expanded="false"
+                                aria-controls="collapseOne">
+                                Messaggio ricevuto da {{ $message['received']->email }} per
+                                l'apparamento: "{{ $message['apartment_name'] }}"
+                            </button>
+                        </h2>
+                        <div id="collapse-{{ $message['received']->id }}" class="accordion-collapse collapse"
+                            aria-labelledby="headingOne" data-bs-parent="#accordionExample" style="">
+                            <div class="accordion-body position-relative">
+                                <div class="mb-2">
+                                    Ricevuto il: {{ date('d/m/Y', strtotime($message['received']->created_at)) }}
+                                    da <strong>{{ $message['received']->email }}</strong>
+
+                                </div>
+                                <div>
+                                    {{ $message['received']->message }}
+                                </div>
+
+                                <button type="button" class="btn btn-danger position-absolute"
+                                    style="top: 15px; right: 15px" data-bs-toggle="modal"
+                                    data-bs-target="#deleteModal-{{ $message['received']->id }}">
+                                    <i class="fa-solid fa-trash text-light"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            </tbody>
             </table>
+
+
         </div>
     @endif
 
