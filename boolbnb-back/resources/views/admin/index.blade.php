dbd37f6d200404d9d0a9188f7deb5acfcc37bfcb
battistaennio <ennio.battista99@gmail.com>
2024-10-24 10:11:42 +0200

    fix: login landing page

--- boolbnb-back/resources/views/admin/index.blade.php
+++ boolbnb-back/resources/views/admin/index.blade.php
@@ -4,11 +4,12 @@
     <div class="container-fluid">
         <div class="row flex-nowrap">
             <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 aside">
-                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 sidenav">
+                <div
+                    class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 sidenav">
                     {{-- User Name --}}
-                    
+
                     <span class="fs-5 py-4 d-none d-sm-inline">
-                        {{ Auth::user()->name }} {{ Auth::user()->surname }} 
+                        {{ Auth::user()->name }} {{ Auth::user()->surname }}
                     </span>
 
                     {{-- Action list: Home, Appartments, Messages --}}
@@ -21,7 +22,7 @@
                         <li class="nav-item">
                             <a href="http://localhost:5174" class="nav-link align-middle px-0">
                                 <i class="fs-4 bi-house">
-                                </i> 
+                                </i>
                                 <span class="ms-1 d-none d-sm-inline">
                                     {{ __('Vai al sito') }}
                                 </span>
@@ -48,7 +49,8 @@
                     </ul>
 
 
-                    <a href="http://localhost:5174" class="nav-link align-middle px-0 py-3" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                    <a href="http://localhost:5174" class="nav-link align-middle px-0 py-3"
+                        onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                         <i class="fs-5 p-1 fa-solid fa-arrow-right-from-bracket">
 
                         </i>
