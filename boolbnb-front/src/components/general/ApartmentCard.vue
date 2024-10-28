9650d6d943d8d11165fe208a8d731f92b8a8c04a
battistaennio <ennio.battista99@gmail.com>
2024-10-28 10:58:52 +0100

    style: add img card loading

--- boolbnb-front/src/components/general/ApartmentCard.vue
+++ boolbnb-front/src/components/general/ApartmentCard.vue
@@ -1,39 +1,43 @@
 <script>
-
-
+import Loader from '../partials/Loader.vue';
 
 export default {
     name: "ApartmentCard",
     props: {
         data: Object
     },
+    components: {
+        Loader
+    },
     data() {
         return {
-
+            isLoaded: false
         }
-
     },
     methods: {
-
+        imgLoad() {
+            this.isLoaded = true;
+        }
     },
-
     mounted() {
-
+        const img = new Image();
+        img.src = this.data.img_path;
+        img.onload = this.imgLoad;
     }
 }
 </script>
 
 <template>
     <router-link :to="{ name: 'dettagli', params: { slug: data.slug } }">
-        <div class="card text-light d-flex justify-content-end align-items-center"
+        <div v-if="isLoaded" class="card text-light d-flex justify-content-end align-items-center"
             :style="{ backgroundImage: 'url(' + data.img_path + ')' }">
             <div class="box text-center p-1">
                 <h5 class="card-title">{{ data.title }}</h5>
             </div>
         </div>
+        <Loader v-else/>
     </router-link>
 </template>
-
 
 <style lang="scss" scoped>
 @use "../../styles/general.scss" as *;
@@ -47,21 +51,6 @@ export default {
     .box {
         width: 100%;
         background-color: rgba(0, 0, 0, 0.2);
-
-
     }
-
-
 }
-
-/* ................................ */
-/* ul {
-    list-style: none;
-}
-
-.box-img {
-    width: 200px;
-    height: 200px;
-
-} */
 </style>
